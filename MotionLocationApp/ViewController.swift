9c6d9734d9a09fd76398d61cb19ca230718ee018
Heitor Ishihara <ishihara.heitor@gmail.com>
2017-05-25 14:12:41 -0300

    App informa latitude e longitude e localiza o usuario

--- MotionLocationApp/ViewController.swift
+++ MotionLocationApp/ViewController.swift
@@ -7,21 +7,62 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     let locationManager: CLLocationManager = CLLocationManager()
+    var here: CLLocation = CLLocation(latitude: 0, longitude: 0)
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Variaves de teste
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Pedir permissao
+        locationManager.requestWhenInUseAuthorization()
+        
+        //Popup de permissao
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            
+            //Configurar precisao
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        //Adicionar regioes
+//        let regiao31 = MKCircle(center: here., radius: 50)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if manager.location!.distance(from: here) > 2{
+            here = manager.location!
+            centerMap(location: here)
+        }
+        
+    
+        latitude.text = ("Latitude: \(here.coordinate.latitude)")
+        longitude.text = ("Longitude: \(here.coordinate.longitude)")
+        
+    }
+    
+    func centerMap(location: CLLocation){
+        //Centraliza o mapa
+        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 100, 100)
+        mapView.setRegion(region, animated: true)
     }
 
 
