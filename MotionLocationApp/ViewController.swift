0934378de59b177a9b0b1b121a963f092bdf618e
Heitor Ishihara <ishihara.heitor@gmail.com>
2017-05-25 13:36:26 -0300

    First commit

---
+++ MotionLocationApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MotionLocationApp
+//
+//  Created by Heitor Ishihara on 24/05/17.
+//  Copyright © 2017 Heitor Ishihara. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import CoreMotion
+
+class ViewController: UIViewController {
+
+    let locationManager: CLLocationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
